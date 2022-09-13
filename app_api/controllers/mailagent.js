1eef818ecd25b48262dd0702a79facedea72eca5
martin <da5407@gmail.com>
2022-09-13 09:54:48 +0800

    Checked mail server existed before sending mail

--- app_api/controllers/mailagent.js
+++ app_api/controllers/mailagent.js
@@ -1,5 +1,6 @@
 //const nodemailer = require("nodemailer");
 const mongoose = require('mongoose');
+const dns = require("dns");
 const crypto = require('crypto');
 const User = mongoose.model('User');
 const mailobj = require('./emailsendinblue');
@@ -22,24 +23,38 @@ const sendMail = (req, res) => {
     const userMailUrl = req.body.email;
     //console.log(userMailUrl);
     //console.log(pws);
-    User.findOne({ email: userMailUrl }, (err, row) => { //查看看練習檔中使用者有無紀錄
-        if (err) {
-            console.log(err);
+    const maildomain = userMailUrl.split('@')[1];//取出mail server的domain name
+    dns.lookup(maildomain, (error, address, family) => { //判斷mail server是否存在
+        if (error) {
+            console.log(error);
+            res.status(400)
+                .json({ "message": `${userMailUrl} 不存在，請用真正的email`, "data": error })
 
-            //return err;
-        }
-        if (row) {
-            done = true;
-            //console.log(done);
-            //console.log(row);
-            row.setPassword(pws);
-            row.save();
-            // return 'Password changed.';
-            setTimeout(() => {
-                if (done){
-                    mailobj.mailOfForgetPws(req,res,pws,row.name)
+        } else {
+            console.log(
+                `The ip address is ${address} and the ip version is ${family}`
+            );
+
+            User.findOne({ email: userMailUrl }, (err, row) => { //查看看練習檔中使用者有無紀錄
+                if (err) {
+                    console.log(err);
+        
+                    //return err;
                 }
-            }, 1000);
+                if (row) {
+                    done = true;
+                    //console.log(done);
+                    //console.log(row);
+                    row.setPassword(pws);
+                    row.save();
+                    // return 'Password changed.';
+                    setTimeout(() => {
+                        if (done){
+                            mailobj.mailOfForgetPws(req,res,pws,row.name)
+                        }
+                    }, 1000);
+                }
+            });
         }
     });
 
@@ -75,40 +90,49 @@ const sendMail = (req, res) => {
 const forgetPws=(req,res)=>{
     const pws = 'vocabulary888';
     const userMailUrl = req.body.email;
-    console.log(userMailUrl);
+    //console.log(userMailUrl);
     //console.log(pws);
-    User.findOne({ email: userMailUrl }, (err, row) => { //查看看練習檔中使用者有無紀錄
-        if (err) {
-            console.log(err);
-            res
-            .status(400)
-            .json({ "message": `${userMailUrl}不是會員的EMail，無法重設密碼`, "data": ""  });
+    const maildomain = userMailUrl.split('@')[1];//取出mail server的domain name
+    dns.lookup(maildomain, (error, address, family) => { //判斷mail server是否存在
+        if (error) {
+            console.log(error);
+            res.status(400)
+                .json({ "message": `${userMailUrl} 不存在，請用真正的email`, "data": error })
 
-        }
-        if (row) {
-            done = true;
-            //console.log(done);
-            //console.log(row);
-            row.setPassword(pws);
-            row.save((err) => {
+        } else {
+            User.findOne({ email: userMailUrl }, (err, row) => { //查看看練習檔中使用者有無紀錄
                 if (err) {
-                console.log(err);
+                    console.log(err);
                     res
-                        .status(409)
-                        .json({ "message": " 重設密碼失敗;"+err, "data": ""  });
-                } else {
-                    mailobj.mailOfForgetPws(req,res,pws,row.name)
+                    .status(400)
+                    .json({ "message": `${userMailUrl}不是會員的EMail，無法重設密碼`, "data": ""  });
+        
+                }
+                if (row) {
+                    done = true;
+                    //console.log(done);
+                    //console.log(row);
+                    row.setPassword(pws);
+                    row.save((err) => {
+                        if (err) {
+                        console.log(err);
+                            res
+                                .status(409)
+                                .json({ "message": " 重設密碼失敗;"+err, "data": ""  });
+                        } else {
+                            mailobj.mailOfForgetPws(req,res,pws,row.name)
+                        }
+                    });
+                }
+                else {
+                    res
+                    .status(403)
+                    .json({ "message": `${userMailUrl}不是會員的EMail，無法重設密碼`, "data": ""  });
                 }
             });
-        }
-        else {
-            res
-            .status(403)
-            .json({ "message": `${userMailUrl}不是會員的EMail，無法重設密碼`, "data": ""  });
+        
         }
     });
-
-
 }
 
 
@@ -116,39 +140,53 @@ const changePws = (req, res) => {
     const userMailUrl = req.body.email;
     const oldPassword = req.body.oldPassword;
     const newPassword = req.body.newPassword;
-    console.log(userMailUrl);
-    console.log(oldPassword);
-    console.log(newPassword);
-    User.findOne({ email: userMailUrl }, (err, row) => { //查看看練習檔中使用者有無紀錄
-        if (err) {
-            console.log(err);
-            return res.status(402).send({ "message": "查無此email" });
-        }
-        if (row) {
-            console.log(row);
-            const verified=row.validPassword(oldPassword);
-            console.log(verified);
-            if (verified) {
-                done = true;
-                row.setPassword(newPassword);
-                row.save((err) => {
-                    if (err) {
+    // console.log(userMailUrl);
+    // console.log(oldPassword);
+    // console.log(newPassword);
+
+    const maildomain = userMailUrl.split('@')[1];//取出mail server的domain name
+    dns.lookup(maildomain, (error, address, family) => { //判斷mail server是否存在
+        if (error) {
+            console.log(error);
+            res.status(400)
+                .json({ "message": `${userMailUrl} 不存在，請用真正的email`, "data": error })
+
+        } else {
+            User.findOne({ email: userMailUrl }, (err, row) => { //查看看練習檔中使用者有無紀錄
+                if (err) {
                     console.log(err);
-                        res
-                            .status(409)
-                            .json({ "message": " 重設密碼失敗;"+err, "data": ""  });
+                    return res.status(402).send({ "message": "查無此email" });
+                }
+                if (row) {
+                    console.log(row);
+                    const verified=row.validPassword(oldPassword);
+                    console.log(verified);
+                    if (verified) {
+                        done = true;
+                        row.setPassword(newPassword);
+                        row.save((err) => {
+                            if (err) {
+                            console.log(err);
+                                res
+                                    .status(409)
+                                    .json({ "message": " 重設密碼失敗;"+err, "data": ""  });
+                            } else {
+                                res
+                                .status(200)
+                                .json({ "message": `密碼已更新為(${newPassword})`, "data": `密碼已更新為(${newPassword})` });
+                    
+                            }
+                        })
                     } else {
-                        res
-                        .status(200)
-                        .json({ "message": `密碼已更新為(${newPassword})`, "data": `密碼已更新為(${newPassword})` });
-            
+                        return res.status(403).send({ "message": "舊密碼不正確" });
                     }
-                })
-            } else {
-                return res.status(403).send({ "message": "舊密碼不正確" });
-            }
+                }
+            });            
         }
+
     });
+
+
     
 
     // setTimeout(() => {
